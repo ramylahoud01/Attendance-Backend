a807ffbdd125ee918d3f8d0da3f5133e6b793b2b
ramylahoud01 <lahoud284@gmail.com>
2024-03-26 11:38:38 +0200

    add header

--- src/app.js
+++ src/app.js
@@ -13,6 +13,7 @@ import Employee from "./model/Employee.js";
 const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://attendance-frontend-on5kxko1c-ramylahoud01s-projects.vercel.app');
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
     res.setHeader("Access-Control-Allow-Headers", "Content-type , Authorization");
