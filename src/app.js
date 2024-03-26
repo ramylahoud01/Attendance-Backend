554628339818edff031e1dbce96b51d967553d15
ramylahoud01 <lahoud284@gmail.com>
2024-03-26 11:13:40 +0200

    test

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import BreakInRoute from "./routes/BreakIn.js";
 import BreakOutRoute from "./routes/BreakOut.js";
 import QrCodeRoute from "./routes/QrCode.js"
 import ScheduleRoute from "./routes/Schedule.js"
+import Employee from "./model/Employee.js";
 const app = express();
 app.use(bodyParser.json());
 app.use((req, res, next) => {
@@ -28,8 +29,15 @@ app.use('/PunchOut', PunchOutRoute)
 app.use('/BreakIn', BreakInRoute)
 app.use('/BreakOut', BreakOutRoute)
 app.use('/QrCode/:QrId', QrCodeRoute)
-app.get('/test', (req, res) => {
-    res.status(200).json({ message: 'This is a test route.' });
+app.get('/test', async (req, res) => {
+    try {
+        const employee = await Employee.findOne({});
+        console.log('Employee', employee);
+        res.status(200).json({ message: 'This is a test route.', employee });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
 });
 app.use((error, req, res, next) => {
     const status = error.statusCode || 500;
