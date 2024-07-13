1f63b5bc2c425c4900145e4a5e440defed42ecec
ramylahoud01 <lahoud284@gmail.com>
2024-07-13 10:54:50 +0300

    modifing

--- src/helper/db.js
+++ src/helper/db.js
@@ -3,13 +3,13 @@ import mongoose from "mongoose";
 //dotenv.config({ path: '.env.local' });
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://Ramy_lh:JBnjceDRWEgkkT9O@atlascluster.xdeoggk.mongodb.net/Attendance-System',
-            {
-                useNewUrlParser: true,
-                useUnifiedTopology: true,
-            }
-        )
-        console.log("Connected to MongoDB...");
+        const conncected = await mongoose.connect('mongodb+srv://Ramy_lh:JBnjceDRWEgkkT9O@atlascluster.xdeoggk.mongodb.net/Attendance-System')
+        if (conncected) {
+            console.log("Connected to MongoDB...");
+            return true;
+        } else {
+            console.log("Not Connected to MongoDB...");
+        }
         return true;
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
